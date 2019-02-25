b73b11ecd88c6fb3b1a255f67cfd53c6e53de6f9
Kirill Goncharov <kdgoncharov@gmail.com>
2019-02-25 19:30:58 +0300

    Implementation of File Backend (#84)

---
+++ celery_once/backends/file.py
@@ -0,0 +1,60 @@
+"""
+Definition of the file locking backend.
+"""
+import errno
+import os
+import tempfile
+import time
+
+from celery_once.tasks import AlreadyQueued
+
+
+class FileBackend(object):
+    """
+    File locking backend.
+    """
+    def __init__(self, settings):
+        self.location = settings.get('location')
+        if self.location is None:
+            self.location = os.path.join(tempfile.gettempdir(),
+                                         'celery_once')
+        try:
+            os.makedirs(self.location)
+        except OSError as error:
+            # Directory exists?
+            if error.errno != errno.EEXIST:
+                # Re-raise unexpected OSError
+                raise
+
+    def _get_lock_path(self, key):
+        return os.path.join(self.location, key)
+
+    def raise_or_lock(self, key, timeout):
+        """
+        Check the lock file and create one if it does not exist.
+        """
+        lock_path = self._get_lock_path(key)
+        try:
+            # Create lock file, raise exception if it exists
+            os.open(lock_path, os.O_CREAT | os.O_EXCL)
+        except OSError as error:
+            if error.errno == errno.EEXIST:
+                # File already exists, check its modification time
+                mtime = os.path.getmtime(lock_path)
+                ttl = mtime + timeout - time.time()
+                if ttl > 0:
+                    raise AlreadyQueued(ttl)
+                else:
+                    # Update modification time if timeout happens
+                    os.utime(lock_path, None)
+                    return
+            else:
+                # Re-raise unexpected OSError
+                raise
+
+    def clear_lock(self, key):
+        """
+        Remove the lock file.
+        """
+        lock_path = self._get_lock_path(key)
+        os.remove(lock_path)
---
+++ tests/unit/backends/test_file.py
@@ -0,0 +1,117 @@
+import errno
+import os
+import tempfile
+import time
+
+import pytest
+
+from celery_once.backends.file import FileBackend
+from celery_once.tasks import AlreadyQueued
+
+
+def test_file_init(mocker):
+    makedirs_mock = mocker.patch('celery_once.backends.file.os.makedirs')
+    location = '/home/test'
+    backend = FileBackend({'location': location})
+
+    assert backend.location == location
+    assert makedirs_mock.called is True
+    assert makedirs_mock.call_args[0] == (location,)
+
+
+def test_file_init_default(mocker):
+    makedirs_mock = mocker.patch('celery_once.backends.file.os.makedirs')
+    backend = FileBackend({})
+
+    assert backend.location == os.path.join(tempfile.gettempdir(),
+                                            'celery_once')
+    assert makedirs_mock.called is True
+
+
+def test_file_init_location_exists(mocker):
+    makedirs_mock = mocker.patch('celery_once.backends.file.os.makedirs',
+                                 side_effect=OSError(errno.EEXIST, 'error'))
+    location = '/home/test'
+    backend = FileBackend({'location': location})
+
+    assert backend.location == location
+    assert makedirs_mock.called is True
+
+
+TEST_LOCATION = '/tmp/celery'
+
+
+@pytest.fixture()
+def backend(mocker):
+    mocker.patch('celery_once.backends.file.os.makedirs')
+    backend = FileBackend({'location': TEST_LOCATION})
+    return backend
+
+
+def test_file_create_lock(backend, mocker):
+    key = 'test.task.key'
+    timeout = 3600
+    open_mock = mocker.patch('celery_once.backends.file.os.open')
+    mtime_mock = mocker.patch('celery_once.backends.file.os.path.getmtime')
+    utime_mock = mocker.patch('celery_once.backends.file.os.utime')
+    expected_lock_path = os.path.join(TEST_LOCATION, key)
+    ret = backend.raise_or_lock(key, timeout)
+
+    assert open_mock.call_count == 1
+    assert open_mock.call_args[0] == (
+        expected_lock_path,
+        os.O_CREAT | os.O_EXCL,
+    )
+    assert utime_mock.called is False
+    assert ret is None
+
+def test_file_lock_exists(backend, mocker):
+    key = 'test.task.key'
+    timeout = 3600
+    open_mock = mocker.patch(
+        'celery_once.backends.file.os.open',
+        side_effect=OSError(errno.EEXIST, 'error'))
+    mtime_mock = mocker.patch(
+        'celery_once.backends.file.os.path.getmtime',
+        return_value=1550155000.0)
+    time_mock = mocker.patch(
+        'celery_once.backends.file.time.time',
+        return_value=1550156000.0)
+    utime_mock = mocker.patch('celery_once.backends.file.os.utime')
+    with pytest.raises(AlreadyQueued) as exc_info:
+        backend.raise_or_lock(key, timeout)
+
+    assert open_mock.call_count == 1
+    assert utime_mock.called is False
+    assert exc_info.value.countdown == timeout - 1000
+
+def test_file_lock_timeout(backend, mocker):
+    key = 'test.task.key'
+    timeout = 3600
+    open_mock = mocker.patch(
+        'celery_once.backends.file.os.open',
+        side_effect=OSError(errno.EEXIST, 'error'))
+    mtime_mock = mocker.patch(
+        'celery_once.backends.file.os.path.getmtime',
+        return_value=1550150000.0)
+    time_mock = mocker.patch(
+        'celery_once.backends.file.time.time',
+        return_value=1550156000.0)
+    utime_mock = mocker.patch('celery_once.backends.file.os.utime')
+    expected_lock_path = os.path.join(TEST_LOCATION, key)
+    ret = backend.raise_or_lock(key, timeout)
+
+    assert open_mock.call_count == 1
+    assert utime_mock.call_count == 1
+    assert utime_mock.call_args[0] == (expected_lock_path, None)
+    assert ret is None
+
+def test_file_clear_lock(backend, mocker):
+    key = 'test.task.key'
+    remove_mock = mocker.patch('celery_once.backends.file.os.remove')
+    expected_lock_path = os.path.join(TEST_LOCATION, key)
+    ret = backend.clear_lock(key)
+
+    assert remove_mock.call_count == 1
+    assert remove_mock.call_args[0] == (expected_lock_path,)
+    assert ret is None

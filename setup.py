02df98355483f5efbe56d12a3741592f2163eba8
Cameron Maske <cameronmaske@gmail.com>
2019-08-21 15:04:25 +0200

    Update pypi listing to reflect 2.7, 3.5, 3.6 and 3.7 support

--- setup.py
+++ setup.py
@@ -42,11 +42,11 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Distributed Computing'
     ],
 )
